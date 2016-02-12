6c3b018b80c79e5a5a505d203a1da0bbd32da78b
Chris Liddell <chrisliddell1991@gmail.com>
2016-02-12 17:22:16 +0000

    Updated gitignore to ignore uploaded files (/uploads). Modified styles.

--- drop/js/display.js
+++ drop/js/display.js
@@ -11,7 +11,7 @@
     success: function ( data ) {
       $( data )
       // all the types
-      .find('tr:nth-child(n+4) > td > a')
+      .find('a[href*=".jpg"], a[href*=".gif"], a[href*=".png"], a[href*=".JPG"]')
       .each( function () {
         var filename = this.href.replace( window.location.host, '' ).replace( 'http://', '' ).replace( '/drop', '');
         $( '.display' ).append( '<img src="' + dir + filename + '">' );
