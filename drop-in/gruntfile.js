b689638e9fc5ec1617a373888b82039faea66ff3
Chris Liddell <chrisliddell1991@gmail.com>
2016-02-12 09:58:15 +0000

    Moved file-upload to separate folder. Starting new feature branch after this commit. Removed unused folders from /yourour

---
+++ drop-in/gruntfile.js
@@ -0,0 +1,60 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+      sass: {
+        files: ['./css/sass/*.scss'],
+        tasks: ['sass:dev', 'autoprefixer:dev']
+      }
+    },
+
+    sass: {
+      dev: {
+        options: {
+          outputStyle: 'nested',
+          sourceMap: true
+        },
+        files: {
+          './css/style.css': './css/sass/style.scss'
+        }
+      }
+    },
+
+    autoprefixer: {
+      dev: {
+        options: {
+          map: true, 
+          browsers: ['ie >= 8', '> 2%']
+        }, 
+        src: './css/style.css',
+        dest: './css/style.css'
+      }
+    }, 
+
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src: [
+            './css/style.css'
+          ]
+        },
+        options: {
+          watchTask: true,
+          // Using a vhost-based url
+          proxy: "sandbox.dev/drop-in"
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+
+  grunt.registerTask('default', ['browserSync', 'watch']);
+};
