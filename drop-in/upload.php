98621cb420250c994461177bc1ec84b479a34d16
Chris Liddell <chrisliddell1991@gmail.com>
2016-02-12 11:09:18 +0000

    Now takes multiple files. Still logging errors in the console.

--- drop-in/upload.php
+++ drop-in/upload.php
@@ -1,50 +1,36 @@
 <?php
 
+// Set some vars
 $target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["uploadedFile"]["name"]);
 $uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+$max_file_size = 50000000;
 
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["uploadedFile"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
+// Loop through each file
+for ( $i = 0; $i < count( $_FILES[ 'uploadedFiles' ][ 'name' ]); $i++ ) {
+  // Get the temp file path
+  $temp_file = $_FILES[ 'uploadedFiles' ][ 'tmp_name' ][ $i ];
+  // Get the target path
+  $target_file = $target_dir . $_FILES[ 'uploadedFiles' ][ 'name' ][ $i ];
+
+  // Check if file already exists
+  if ( file_exists( $target_file ) ) {
+    echo 'Sorry, ' . $target_file . ' already exists. ';
     $uploadOk = 0;
   }
-}
 
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
+  // Check file size
+  if ($_FILES[ 'uploadedFiles' ][ 'size' ][ $i ] > $max_file_size) {
+    echo 'Sorry, your file is too large.';
     $uploadOk = 0;
-}
+  }
 
-// Check file size
-if ($_FILES["uploadedFile"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
+  // Make sure we have a filepath
+  if ( $temp_file != '' && $uploadOk = 1) {
 
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["uploadedFile"]["tmp_name"], $target_file)) {
-    echo "The file ". basename( $_FILES["uploadedFile"]["name"]). " has been uploaded.";
-  } else {
-    echo "Sorry, there was an error uploading your file.";
+    // Upload to dir
+    if ( move_uploaded_file( $temp_file, $target_file ) ) {
+      echo 'File uploaded! Alright!';
+    }
   }
 }
 
