38c57aa2da46f0f10d199b9b304b40b81bc1c49b
Chris Liddell <chrisliddell1991@gmail.com>
2016-02-24 18:26:17 +0000

    Created index.html main.js

---
+++ angular2-quickstart/app/main.js
@@ -0,0 +1,9 @@
+( function( app ) {
+
+  document.addEventListener( 'DOMContentLoaded', function() {
+
+    ng.platform.browser.bootstrap( app.AppComponent );
+
+  } );
+  
+} )( window.app || ( window.app = {} ) );
